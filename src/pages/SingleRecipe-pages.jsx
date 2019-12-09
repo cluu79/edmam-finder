51e468143c0a58467b1b5c720ee7852c182fd5a1
Danny Luu <cluu79@gmail.com>
2019-12-09 15:13:15 -0600

    added fake data

--- src/pages/SingleRecipe-pages.jsx
+++ src/pages/SingleRecipe-pages.jsx
@@ -1,12 +1,77 @@
 import React, { Component } from 'react';
+import { recipeData } from '../tempData/recipeData-temp'
+import {Link} from 'react-router-dom'
 
 class SingleRecipe extends Component {
+  constructor(props) {
+    super(props);
+    const id = this.props.match.params.id;
+    this.state={
+      recipe: recipeData,
+      id, 
+      loading:false
+    }
+
+  }
+
   render() {
-    return (
-      <h4 className="text-success">
-        Hello from Single Recipe page
+    const { image_url, publisher, publisher_url, source_url, title, ingredients } = this.state.recipe;
+    if (this.state.loading) {
+      return (<div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-6 my-3">
+            <h2 className="text-uppercase text-center text-orange">loading hahahaha....</h2>
+          </div>
+        </div>
         
-      </h4>
+      </div>
+      )
+    }
+    return (
+      <div className='container my-5'>
+        <div className='row'>
+          <div className='col-10 mx-auto col-md-6 my-3'>
+            <Link to='/recipes' className='btn btn-danger mb-5'>
+              back to recipes list
+            </Link>
+            <img
+              src={image_url}
+              className='d-block w-100'
+              style={{ maxHeight: '30rem' }}
+              alt='recipe'
+            />
+          </div>
+          <div className='col-10 mx-auto col-md-6 my-3'>
+            <h6 className='text-uppercase'>{title}</h6>
+            <h6 className='text-warning text-capitalize'>
+              provided by: {publisher}
+            </h6>
+            <a
+              href={publisher_url}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='btn btn-primary mt-2 text-capitalize'
+            >
+              publisher webpage
+            </a>
+
+            <a
+              href={source_url}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='btn btn-success mt-2 text-capitalize'
+            >
+            recipe URL
+            </a>
+            <ul className="list-group mt-4">
+              <h2 className="mt-3 mb-4">ingredients</h2>
+              {ingredients.map((item, index) => {
+                return (<li key={index} className="list-group-item">{item}</li>)
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     );
   }
 }
---
+++ src/tempData/recipeData-temp.js
@@ -0,0 +1,18 @@
+export const recipeData = {
+         edmam_url:
+           'https://www.edamam.com/recipe/california-sandwich-469b6ac813f040c5336aa3610c0a74a1/sandwich',
+         image_url:
+           'https://www.edamam.com/web-img/84a/84acce2c648f39994c68740681883dd5.jpg',
+         ingredients: [
+           '2 some salt and some peper',
+           '2 slices of bread',
+           '1 tablespoon of some superman stuff',
+           '1/2 cup of some captain America juice',
+           "1/5 cup of some toney starks's brain juice"
+         ],
+         publisher: 'edamamel',
+         publisher_url:
+           'https://www.edamam.com/recipe/california-sandwich-469b6ac813f040c5336aa3610c0a74a1/sandwich',
+         source_url:
+           'https://www.edamam.com/recipe/california-sandwich-469b6ac813f040c5336aa3610c0a74a1/sandwich'
+       };
