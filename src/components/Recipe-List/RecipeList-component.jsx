55c988da8989569be3062920943292957d7d8a2a
Danny Luu <cluu79@gmail.com>
2019-12-09 13:15:28 -0600

    created card page

--- src/components/Recipe-List/RecipeList-component.jsx
+++ src/components/Recipe-List/RecipeList-component.jsx
@@ -3,11 +3,24 @@ import Recipe from '../Recipe/Recipe-component';
 
 export default class RecipeList extends Component {
   render() {
+    const { recipes } = this.props;
     return (
-    <div>
-      Helo from recipe list
-      <Recipe />
-      </div>
+      <div>
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-capitalize mb-3">
+              <h1 className="text-slanted">recipe List</h1>
+            </div>
+          </div>
+
+          <div className="row">
+            {recipes.map(recipe => (<Recipe key={recipe.recipe_id} recipe={recipe} />))}
+          </div>
+
+          
+
+        </div>
+     </div>
     )
     
   }
